9c57e186e1a983d0b17797bc7d42703a359edbc2
Ashwini Yadrami <yadramiash192380@gmail.com>
2024-10-09 21:58:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AnanymousMethod
+{
+    public  class AnanymousMethod
+    {
+        public delegate string PrintDelegate(string name);     //declaring delegate function
+       
+        static void Main(string[] args)
+        {
+            string Name = "Avinash";
+            PrintDelegate p = delegate (string name)          //ananymous object using delegate function
+            {
+                Console.WriteLine($"Name of the Singer is:{Name}");
+                return name;
+            };
+            string str = p("Ashwini");
+            Console.WriteLine("name is:"+str);   
+            
+        }
+    }
+}
